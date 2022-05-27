60d1166b273f92282cd8c3a227894d7a534481c5
77x17 <phanngung77x17@gmail.com>
2022-05-27 16:59:59 +0700

    firstUpload

---
+++ learnPython/L3DataStructure/Cau2.py
@@ -0,0 +1,9 @@
+A = 110
+B = 262
+
+print("{}  +  {} = {}".format(A, B, A +  B))
+print("{}  -  {} = {}".format(A, B, A -  B))
+print("{}  /  {} = {}".format(A, B, A /  B))
+print("{}  *  {} = {}".format(A, B, A *  B))
+print("{} div {} = {}".format(A, B, A // B))
+print("{} mod {} = {}".format(A, B, A %  B))
