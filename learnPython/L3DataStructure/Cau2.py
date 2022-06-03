93746c10f1d23c2c4684ba4af4b633c7699935ce
77x17 <72738828+77x17@users.noreply.github.com>
2022-06-03 20:30:22 +0700

    Delete learnPython directory

--- learnPython/L3DataStructure/Cau2.py
+++
@@ -1,9 +0,0 @@
-A = 110
-B = 262
-
-print("{}  +  {} = {}".format(A, B, A +  B))
-print("{}  -  {} = {}".format(A, B, A -  B))
-print("{}  /  {} = {}".format(A, B, A /  B))
-print("{}  *  {} = {}".format(A, B, A *  B))
-print("{} div {} = {}".format(A, B, A // B))
-print("{} mod {} = {}".format(A, B, A %  B))
--- learnPython/L3DataStructure/Cau3.py
+++
@@ -1,9 +0,0 @@
-st = "Python programming is the best course to learn"
-
-print("a. ", len(st))
-print("b. ", st[:5])
-print("c. ", st[-10:])
-print("d. ", st.replace("Python", "DUT"))
-print("e. ", st.find("best"))
-print("f. ", st.islower())
-print("g. ", st.split())
